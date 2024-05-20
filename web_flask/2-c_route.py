80c7dc1c5fd3482527bf5bd9290dd34e75e0c34c
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-20 16:09:15 +0100

    Document all functions in the script.

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,14 +7,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """ The home page. """
     return "Hello HBNB!"
 
 @app.route("/hbnb")
 def home_hbnb():
+    """ The HBNB page. """
     return "HBNB"
 
 @app.route("/c/<text>")
 def c_display(text):
+    """ 
+    The C display page.
+
+    Parameter
+    ---------
+        text: the last string of the url
+    """
     return f"C {text.replace("_", " ")}"
 
 
