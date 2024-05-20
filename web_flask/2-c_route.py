03d5264819570c0bc1babb63916f80d9cddbabf1
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-20 15:56:27 +0100

    Add script that starts a Flask web application.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application. """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def home_hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>")
+def c_display(text):
+    return f"C {text.replace("_", " ")}"
+
+
+if __name__ == "__main__":
+    app.run()
