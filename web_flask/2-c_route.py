39ba217d9cd4aa74f6f509483cfd576929dc33e0
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-20 16:11:18 +0100

    Conform codebase to pycodestyle.

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,14 +10,16 @@ def home():
     """ The home page. """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb")
 def home_hbnb():
     """ The HBNB page. """
     return "HBNB"
 
+
 @app.route("/c/<text>")
 def c_display(text):
-    """ 
+    """
     The C display page.
 
     Parameter
