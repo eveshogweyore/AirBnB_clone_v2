447c12629a31d3cd801e27c86de5872830e99747
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-20 16:44:11 +0100

    Make the F-String work properly.

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,20 +7,18 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
-    """home route"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def home_hbnb():
-    """hbnb return route"""
     return "HBNB"
 
-@app.route("/c/", strict_slashes=False)
+
 @app.route("/c/<text>", strict_slashes=False)
 def cdisplay(text):
     """ Display text with C. """
-    return "C {text.replace('_', ' ')}"
+    return f"C {text.replace('_', ' ')}"
 
 
 if __name__ == "__main__":
