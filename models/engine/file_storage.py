0351c7289a5c32fcf35d1bd092d960a556ead4ba
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-25 23:37:08 +0100

    Add delete() method to delete an existing and add feature to all() method.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,19 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+
+        customized_objects = {}
+        #print(f"cls.__name)__: {cls.__name__}")
+        for key, value in FileStorage.__objects.items():
+            if cls.__name__ in key:
+                #print(f"key: {key}");
+                customized_objects[key] = value
+
+        return customized_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +58,14 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        get_key = ''
+        if obj is not None:
+            for key, value in FileStorage.__objects.items():
+                if obj.id in key:
+                    get_key = key
+                    break
+
+            if len(get_key) > 0:
+                del FileStorage.__objects[get_key]
